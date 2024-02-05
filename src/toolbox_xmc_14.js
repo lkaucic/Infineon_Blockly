4a21d29c9e14f97a7ee4954cebbde0b81ae311cb
Luka Kaučić <73308606+lkaucic@users.noreply.github.com>
2024-02-05 10:12:53 +0100

    Fix toolboxes

--- src/toolbox_xmc_14.js
+++ src/toolbox_xmc_14.js
@@ -1,18 +1,120 @@
 export const toolbox_xmc_14 = {
-    'kind': 'flyoutToolbox',
-    'contents': [
-   
-      {
-        'kind': 'block',
-        'type': 'variables_sett'
-      },
-      {
-        'kind': 'block',
-        'type': 'macro_sett'
-      },
-    ]
-  }
-  
-  
-  
+  'kind': 'flyoutToolbox',
+  'contents': [
+ 
+    {
+      'kind': 'block',
+      'type': 'variables_sett'
+    },
+    {
+      'kind': 'block',
+      'type': 'macro_sett'
+    },
+    {
+      'kind': 'block',
+      'type': 'volatile_wrap'
+    },
+    {
+      'kind': 'block',
+      'type': 'operation'
+    },
+    {
+      'kind': 'block',
+      'type': 'operation2'
+    },
+    {
+      'kind': 'block',
+      'type': 'math_number'
+    },
+    {
+      'kind': 'block',
+      'type': 'arith_operation'
+    },
+    {
+      'kind': 'block',
+      'type': 'main'
+    },
+    {
+      'kind': 'block',
+      'type': 'while_do'
+    },
+    {
+      'kind': 'block',
+      'type': 'controls_if'
+    },
+    {
+      'kind': 'block',
+      'type': 'condition'
+    },
+    {
+      'kind': 'block',
+      'type': 'variables_get'
+    },
+    {
+      'kind': 'block',
+      'type': 'for_loop'
+    },
+   {
+      'kind': 'block',
+      'type': 'pin_set',
+      'comment' : 'config'
+   },
+   {
+    'kind': 'block',
+    'type': 'pin1_set',
+  },
+   {
+    'kind': 'block',
+    'type': 'input_type'
+  },
+  {
+    'kind': 'block',
+    'type': 'output_type'
+  },
+  {
+    'kind': 'block',
+    'type': 'init'
+  },
+  {
+    'kind': 'block',
+    'type': 'gpio_init'
+  },
+  {
+    'kind': 'block',
+    'type': 'gpio_set_mode'
+  },
+  {
+    'kind': 'block',
+    'type': 'gpio_set_output_level'
+  },
+  {
+    'kind': 'block',
+    'type': 'procedures_defreturn'
+  },
+  {
+    'kind': 'block',
+    'type': 'text'
+  },
+  {
+    'kind': 'block',
+    'type': 'toggle'
+  },
+  {
+    'kind': 'block',
+    'type': 'logical_cond'
+  },
+  {
+    'kind': 'block',
+    'type': 'interrupt'
+  },
+  {
+    'kind': 'block',
+    'type': 'delay'
+  },
+  ]
+}
+
+
+
+
   
--- src/toolbox_xmc_47.js
+++ src/toolbox_xmc_47.js
@@ -22,26 +22,6 @@ export const toolbox_xmc_47 = {
         'kind': 'block',
         'type': 'operation2'
       },
-      {
-        'kind': 'block',
-        'type': 'math_number'
-      },
-      {
-        'kind': 'block',
-        'type': 'arith_operation'
-      },
-      {
-        'kind': 'block',
-        'type': 'main'
-      },
-      {
-        'kind': 'block',
-        'type': 'while_do'
-      },
-      {
-        'kind': 'block',
-        'type': 'controls_if'
-      },  
     ]
   }
   
